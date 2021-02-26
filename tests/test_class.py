711b986dad44b76f4522e5f3e11561851ff9b18f
MichaelBroox <michael.holdbrooke.brx@gmail.com>
2021-02-26 11:31:13 +0000

    Add test

---
+++ tests/test_class.py
@@ -0,0 +1,14 @@
+import unittest
+import app
+
+class TestApp(unittest.TestCase):
+
+    def setUp(self):
+        self.app = app.test_client()
+    
+    def tearDown(self):
+        pass
+
+    def test_page(self):
+        response = self.app.get('/about', follow_redirects=True)
+        self.assertEqual(response.status_code, 200)
